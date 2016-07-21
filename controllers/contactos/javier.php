b2de1e9b6a7d190ed5608aa64bfce2a2ebe4112f
Luis Davis <ldavis@intellisys.com.do>
2016-07-21 17:17:03 -0400

    cleaning and adding new page

---
+++ controllers/contactos/javier.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$language = "Javier";
+$title = "this is a simple Javier";
+
+view('contactos/javier', array('language' => $language, 'title' => $title));
+
+?>
+
--- index.php
+++ index.php
@@ -3,11 +3,5 @@
 require 'config.php';
 require 'helper.php';
 
-// function controller($name){
-// 	require 'controllers/$name.php';
-// }
 
-// if (!isset(($_GET['url'])) {
-// 	// $_GET['url'] = 'main';
-// }
 run_controller($_GET['url']);
